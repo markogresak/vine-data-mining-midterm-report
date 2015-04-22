de87cb64fd59723d1cebe9a4a130f8c8368c77a7
Marko Grešak <marko@gresak.io>
2015-04-22 03:57:14 +0200

    add bower_components to static dir

--- js/plugin/notes-server/index.js
+++ js/plugin/notes-server/index.js
@@ -28,7 +28,7 @@ io.sockets.on( 'connection', function( socket ) {
 
 app.configure( function() {
 
-	[ 'css', 'js', 'images', 'plugin', 'lib' ].forEach( function( dir ) {
+	[ 'css', 'js', 'images', 'plugin', 'lib', 'bower_components' ].forEach( function( dir ) {
 		app.use( '/' + dir, staticDir( opts.baseDir + dir ) );
 	});
 
